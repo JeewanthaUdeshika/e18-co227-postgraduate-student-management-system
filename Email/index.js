ca004edfe1f3ccdb645971c8eca4af5a7e17f8ce
Ishan Maduranga <e18173@eng.pdn.ac.lk>
2022-06-17 11:42:18 +0530

    Added the Options to send BCC and CC mails

--- Email/index.js
+++ Email/index.js
@@ -4,11 +4,15 @@
  * Automated Mail Sender
  * 17/06/2022
  */
+
 const nodemailer = require("nodemailer");
 const secure_configuration = require("./secure");
 
 //All the security_configs are in a seperate file for security purposes
 const transporter = nodemailer.createTransport({
+  // host: "smtp.gmail.com",
+  // port: 465,
+  // secure: true,
   service: "gmail",
   auth: {
     type: "OAuth2",
@@ -17,16 +21,19 @@ const transporter = nodemailer.createTransport({
     clientId: secure_configuration.CLIENT_ID,
     clientSecret: secure_configuration.CLIENT_SECRET,
     refreshToken: secure_configuration.REFRESH_TOKEN,
-    // accessToken: secure_configuration.ACCESS_TOKEN,
   },
 });
 
 const mailConfigurations = {
   // It should be a string of sender email
-  from: secure_configuration.EMAIL_USERNAME,
+  from: `Ishan Maduranga ${secure_configuration.EMAIL_FROM}`,
 
   // Comma Separated list of mails
-  to: secure_configuration.EMAIL_LIST,
+  to: secure_configuration.EMAIL_LIST_TO,
+
+  // send the list as bcc or cc
+  // bcc: secure_configuration.EMAIL_LIST_BCC,
+  // cc: secure_configuration.EMAIL_LIST_CC,
 
   // Subject of Email
   subject: "Sending Email using Node.js",
@@ -36,6 +43,11 @@ const mailConfigurations = {
     "Hi! There, You know I am using the" +
     " NodeJS Code along with NodeMailer " +
     "to send this email.",
+
+  headers: {
+    priority: "high",
+  },
+
   attachments: [
     {
       filename: "test.txt",
