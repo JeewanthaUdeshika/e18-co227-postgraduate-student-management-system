97ad85cb2edeb76fd4994f1ad0cdb4275af786fa
Ishan Maduranga <e18173@eng.pdn.ac.lk>
2022-06-17 09:40:30 +0530

    Automatic Mail Sender v1.0

---
+++ Email/README.md
@@ -0,0 +1,15 @@
+# Postgraduate Student Management System
+The aim of this project is to design and deployment of a student management system for PG
+students in the Department of Computer Engineering.
+
+## Features:
+1. Student Registration System
+2. User Management facility for Administrators (PG Coordinator, HoD, Internal Staff, and
+External Staff) with different privileges
+3. Monitor applications of prospective students
+4. Monitor Progress of enrolled students (both course works and research projects)
+5. System-generated emails/notifications on important deadlines, the progress of
+students, and jobs assigned for various stakeholders including
+reviewers/examiners/superviors/etc
+6. Repository for Mphil/Ph.D. theses and publications
+7. API to other web applications (e.g. Departments LMS, Web site, AR Office etc)
---
+++ Email/index.js
@@ -0,0 +1,52 @@
+/**
+ * E / 18 / 173
+ * Kasthuripitiya K.A.I.M.
+ * Automated Mail Sender
+ * 17/06/2022
+ */
+const nodemailer = require("nodemailer");
+const secure_configuration = require("./secure");
+
+//All the security_configs are in a seperate file for security purposes
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: secure_configuration.EMAIL_USERNAME,
+    pass: secure_configuration.PASSWORD,
+    clientId: secure_configuration.CLIENT_ID,
+    clientSecret: secure_configuration.CLIENT_SECRET,
+    refreshToken: secure_configuration.REFRESH_TOKEN,
+    // accessToken: secure_configuration.ACCESS_TOKEN,
+  },
+});
+
+const mailConfigurations = {
+  // It should be a string of sender email
+  from: "ishankasthuripitiya@gmail.com",
+
+  // Comma Separated list of mails
+  to: "ishanmaduranga271@gmail.com",
+
+  // Subject of Email
+  subject: "Sending Email using Node.js",
+
+  // This would be the text of email body
+  text:
+    "Hi! There, You know I am using the" +
+    " NodeJS Code along with NodeMailer " +
+    "to send this email.",
+  attachments: [
+    {
+      filename: "test.txt",
+      content: "Hello, Mail For Testing",
+    },
+  ],
+};
+
+// Send Mail to the given list of mails , if sending fails log it to the console
+transporter.sendMail(mailConfigurations, (error, info) => {
+  if (error) throw error;
+  console.log("Email Sent Successfully");
+  console.log(info);
+});
