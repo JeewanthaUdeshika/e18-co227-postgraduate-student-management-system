bc529999a6e1b3bad4d3e2bdb3cef442673782ce
Ishan Maduranga <e18173@eng.pdn.ac.lk>
2022-06-18 23:51:06 +0530

    Created a MailSender class and load the sensitive data from .env file

--- Email/index.js
+++ Email/index.js
@@ -6,51 +6,62 @@
  */
 
 const nodemailer = require("nodemailer");
-const secure_configuration = require("./secure");
+require("dotenv").config(); //Read the .env file
 
-//All the security_configs are in a seperate file for security purposes
-const transporter = nodemailer.createTransport({
-  host: secure_configuration.HOST,
-  port: secure_configuration.PORT,
-  secure: false,
-  service: secure_configuration.SERVICE,
-  auth: {
-    user: secure_configuration.EMAIL_USERNAME,
-    pass: secure_configuration.PASSWORD,
-  },
-});
+class MailSender {
+  // Constructor to set the email list
+  constructor(EMAIL_LIST) {
+    this.EMAIL_LIST = EMAIL_LIST;
+  }
 
-const mailConfigurations = {
-  // It should be a string of sender email
-  from: `Ishan Maduranga ${secure_configuration.EMAIL_FROM}`,
+  sendEmail() {
+    //All the security_configs are in a seperate file for security purposes
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST,
+      port: process.env.PORT,
+      secure: false,
+      service: process.env.SERVICE,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.PASSWORD,
+      },
+    });
 
-  // Comma Separated list of mails
-  to: secure_configuration.EMAIL_LIST_TO,
+    const mailConfigurations = {
+      // It should be a string of sender email
+      from: `Ishan Maduranga ${process.env.EMAIL_USERNAME}`,
 
-  // send the list as bcc or cc
-  // bcc: secure_configuration.EMAIL_LIST_BCC,
-  // cc: secure_configuration.EMAIL_LIST_CC,
+      // Comma Separated list of mails
+      to: this.EMAIL_LIST,
 
-  // Subject of Email
-  subject: "Sending Email using Node.js",
+      // send the list as bcc or cc
+      // bcc: secure_configuration.EMAIL_LIST_BCC,
+      // cc: secure_configuration.EMAIL_LIST_CC,
 
-  // This would be the text of email body
-  text:
-    "Hi! There, You know I am using the" +
-    " NodeJS Code along with NodeMailer " +
-    "to send this email.",
+      // Subject of Email
+      subject: "Sending Email using Node.js",
 
-  attachments: [
-    {
-      filename: "test.txt",
-      content: "Hello, Mail For Testing",
-    },
-  ],
-};
+      // This would be the text of email body
+      text:
+        "Hi! There, You know I am using the" +
+        " NodeJS Code along with NodeMailer " +
+        "to send this email.",
 
-// Send Mail to the given list of mails , if sending fails log it to the console
-transporter.sendMail(mailConfigurations, (error, info) => {
-  if (error) throw error;
-  console.log("Email Sent Successfully");
-  console.log(info);
-});
+      attachments: [
+        {
+          filename: "test.txt",
+          content: "Hello, Mail For Testing",
+        },
+      ],
+    };
+
+    // Send Mail to the given list of mails , if sending fails log it to the console
+    transporter.sendMail(mailConfigurations, (error, info) => {
+      if (error) throw error;
+      console.log("Email Sent Successfully");
+      console.log(info);
+    });
+  }
+}
+
+module.exports = MailSender;
