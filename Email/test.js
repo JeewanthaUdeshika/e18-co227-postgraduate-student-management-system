9e575de8ce0c23a5d56c2ceef5a62441fb15a9c7
Ishan Maduranga <e18173@eng.pdn.ac.lk>
2022-06-19 12:07:31 +0530

    index.js -> mailsender.js , test.js -> index.js

--- Email/test.js
+++
@@ -1,7 +0,0 @@
-const MailSender = require("./index");
-
-// Give the mail addresses as strings seperated by commas
-let EMAIL_LIST = [];
-const mailsender = new MailSender(EMAIL_LIST);
-
-mailsender.sendEmail();
