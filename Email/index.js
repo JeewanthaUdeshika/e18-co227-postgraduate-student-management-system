70c79a188a294a5c97a07cf3395be906e4b7f009
Ishan Maduranga <e18173@eng.pdn.ac.lk>
2022-06-21 19:59:38 +0530

    Problems solved with scheduling tasks

--- Email/index.js
+++
@@ -1,8 +0,0 @@
-import moment from "moment";
-import { TaskScheduler } from "./taskScheduler.js";
-const time = moment().format();
-console.log(moment(time).format("HH:mm:ss"));
-const mytask = new TaskScheduler(time);
-
-// Start all the tasks
-mytask.start();
--- Email/jobs/sendEmail.js
+++
@@ -1,7 +0,0 @@
-import { MailSender } from "./mailsender.js";
-
-// Give the mail addresses as strings seperated by commas
-let emailList = ["ishanmaduranga271@gmail.com", "e18173@eng.pdn.ac.lk"];
-const mailsender = new MailSender(emailList);
-
-mailsender.sendEmail();
