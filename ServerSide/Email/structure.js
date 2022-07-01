034864f1b41e3d29d0d366d7f395fad200f8c289
IshanMK <e18173@eng.pdn.ac.lk>
2022-07-01 20:06:08 +0530

    Removed Unwanted Folder

--- ServerSide/Email/structure.js
+++
@@ -1,8 +0,0 @@
-const content = {
-  admin: `New user is Applied.`,
-  notify: `You have 2 weeks remaining for submission`,
-  regSuccess: `Registration was successfull`,
-  unkown: "Unknown Content",
-};
-
-export default content;
