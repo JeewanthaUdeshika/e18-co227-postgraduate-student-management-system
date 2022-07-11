60f4100e4888f014a5e4b92ad7e8aac14046c78b
IshanMK <e18173@eng.pdn.ac.lk>
2022-07-11 09:49:18 +0530

    Update , Forget , Reset Password functionalitites

---
+++ Auth/userRoutes.js
@@ -0,0 +1,30 @@
+// Decide which function needs to be called based on the type of the http req and the url
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+// router.route('/signup').post(authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch(
+  '/updatePassword',
+  authController.protect,
+  authController.updatePassword
+);
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
